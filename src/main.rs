8bfd5e13ccc74ece627e155d1a6edaaf2b2353c0
moonfeather <moonbite233@gmail.com>
2025-04-08 11:51:25 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+mod types;
+use types::Arcade;
+
+use mongodb::{
+    Client, Collection, IndexModel, bson::Bson::Int32, bson::Document, bson::doc,
+    bson::oid::ObjectId, options::IndexOptions,
+};
+use salvo::{oapi::extract::JsonBody, prelude::*};
+use std::sync::OnceLock;
+
+use thiserror::Error;
+
+const DB_NAME: &str = "maimap";
+
+// Custom error type for MongoDB operations
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("MongoDB Error")]
+    ErrorMongo(#[from] mongodb::error::Error),
+}
+static MONGODB_CLIENT: OnceLock<Client> = OnceLock::new();
+#[inline]
+pub fn get_mongodb_client() -> &'static Client {
+    MONGODB_CLIENT.get().unwrap()
+}
+
+#[handler]
+async fn get_arcades_by_id(req: &mut Request, res: &mut Response) {
+    let arcade_id = Int32(req.param::<i32>("arcade_id").unwrap_or_default());
+
+    println!("{}", arcade_id);
+    let client = get_mongodb_client();
+    let coll_arcades: Collection<Arcade> = client.database(DB_NAME).collection("arcades");
+
+    match coll_arcades.find_one(doc! { "arcade_id": arcade_id }).await {
+        Ok(Some(arcade)) => res.render(Json(arcade)),
+        Ok(None) => res.render(Json({})),
+        Err(e) => {
+            res.status_code(StatusCode::BAD_REQUEST);
+            res.render(Json(serde_json::json!({
+                "error": format!("{:?}",e)
+            })))
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let mongodb_uri = "mongodb://localhost:27017/";
+    let client = Client::with_uri_str(mongodb_uri)
+        .await
+        .expect("failed to connect");
+    MONGODB_CLIENT.set(client).unwrap();
+
+    let router =
+        Router::with_path("arcades").push(Router::with_path("{arcade_id}").get(get_arcades_by_id));
+
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    Server::new(acceptor).serve(router).await;
+}
---
+++ src/types.rs
@@ -0,0 +1,26 @@
+use mongodb::bson::oid::ObjectId;
+use mongodb::bson::{DateTime, Decimal128};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Arcade {
+    pub _id: Option<ObjectId>,
+    /// 机厅地址
+    pub arcade_address: String,
+    /// 单局花销
+    pub arcade_cost: f64,
+    /// 机台数量
+    pub arcade_count: i32,
+    /// 机厅存活情况
+    pub arcade_dead: bool,
+    /// 机厅ID
+    pub arcade_id: i32,
+    /// 机厅纬度
+    pub arcade_lat: Decimal128,
+    /// 机厅经度
+    pub arcade_lng: Decimal128,
+    /// 机厅名
+    pub arcade_name: String,
+    /// 创建时间
+    pub created_at: DateTime,
+}
